0dd053c7d922bead7d1b6917e508aa98fb68d643
vikkoooo <vikkolundberg@gmail.com>
2021-12-15 17:02:18 +0100

    added a unity event

--- Assets/Scripts/ScreenWrapper.cs
+++ Assets/Scripts/ScreenWrapper.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScreenWrapper : MonoBehaviour
 {
+	// Screen wrapping
 	private float left = Screen.width;
 	private float right = Screen.width;
 	private float bottom = Screen.height;
@@ -12,38 +14,52 @@ public class ScreenWrapper : MonoBehaviour
 	private float buffer = 0.5f;
 	private Camera cam;
 	private float zDist;
+
+	// Events
+	private UnityEvent myEvent;
 	
 	private void Start()
     {
-		cam = Camera.main;
+	    // Screen wrapping
+	    cam = Camera.main;
 		zDist = Mathf.Abs(cam.transform.position.z + transform.position.z);
 		left = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, zDist)).x;
 		right = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, zDist)).x;
 		bottom = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, zDist)).y;
 		top = cam.ScreenToWorldPoint(new Vector3(0.0f, Screen.height, zDist)).y;
+		
+		// Events
+		myEvent = new UnityEvent();
+		myEvent.AddListener(ListeningToEvent);
     }
 
     private void FixedUpdate()
     {
 	    if (transform.position.x < left - buffer)
 	    {
-		    Debug.Log("Left");
 		    transform.position = new Vector3(right, transform.position.y, transform.position.z);
+		    myEvent.Invoke();
 	    }
 	    if (transform.position.x > right + buffer)
 	    {
-		    Debug.Log("Right");
 		    transform.position = new Vector3(left, transform.position.y, transform.position.z);
+		    myEvent.Invoke();
 	    }
 	    if (transform.position.y < bottom - buffer)
 	    {
-		    Debug.Log("Bottom");
 		    transform.position = new Vector3(transform.position.x, top, transform.position.z);
+		    myEvent.Invoke();
 	    }
 	    if (transform.position.y > top + buffer)
 	    {
-		    Debug.Log("Top");
 		    transform.position = new Vector3(transform.position.x, bottom, transform.position.z);
+		    myEvent.Invoke();
 	    }
     }
+
+    private void ListeningToEvent()
+    {
+	    Debug.Log("Player out of bounds");
+    }
+    
 }
