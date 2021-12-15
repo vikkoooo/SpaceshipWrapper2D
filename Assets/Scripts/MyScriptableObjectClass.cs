28c263294031868e6f17a9f11977a3416f347432
vikkoooo <vikkolundberg@gmail.com>
2021-12-15 16:17:33 +0100

    added scriptable objects from unity learn

--- Assets/Scripts/MyScriptableObjectClass.cs
+++ Assets/Scripts/MyScriptableObjectClass.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "fileName", menuName = "menuName", order = 1)]
+//fileName, menuName, order is optional.
+[CreateAssetMenu(fileName = "Data", menuName = "Inventory/List", order = 1)]
 public class MyScriptableObjectClass : ScriptableObject
 {
 	public string objectName = "New MyScriptableObject";
 	public bool colorIsRandom = false;
 	public Color thisColor = Color.white;
 	public Vector3[] spawnPoints;
-
 }
